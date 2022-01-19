41097823378fa4ffedb3ead96429f0c100b8490d
MA Dingjie <mdj2812@hotmail.com>
2022-01-19 10:40:12 +0800

    Add divider between each demo

--- main.cpp
+++ main.cpp
@@ -8,39 +8,55 @@
 int main(int, char **)
 {
     // Three way comparison
-    // cpp17::threeCompDemo();
-    // cpp20::threeWayCompDemo();
-    // cpp17::threeCompOperatorOverloadDemo();
-    // cpp20::threeWayOperatorOverloadDemo();
+    std::cout << "************ Three way comparison ************\n";
+    cpp17::threeCompDemo();
+    cpp20::threeWayCompDemo();
+    cpp17::threeCompOperatorOverloadDemo();
+    cpp20::threeWayOperatorOverloadDemo();
+    std::cout << "**********************************************\n\n";
 
     // Designated initializer
-    // cpp17::designatedInitializerDemo();
-    // cpp20::designatedInitializerDemo();
+    std::cout << "*********** Designated initializer ***********\n";
+    cpp17::designatedInitializerDemo();
+    cpp20::designatedInitializerDemo();
+    std::cout << "**********************************************\n\n";
 
     // init-statement in range based loop
-    // cpp17::addLineNumber();
-    // cpp20::addLineNumber();
-    // cpp17::danglingReference();
-    // cpp20::ubFree();
+    std::cout << "* init-statement in range based loop *\n";
+    cpp17::addLineNumber();
+    cpp20::addLineNumber();
+    cpp17::danglingReference();
+    cpp20::ubFree();
+    std::cout << "**********************************************\n\n";
 
     // constexpr virtual function
-    // cpp17::constVirtualFunctionDemo();
-    // cpp20::constexprVirtualFunctionDemo();
+    std::cout << "********** constexpr virtual function ********\n";
+    cpp17::constVirtualFunctionDemo();
+    cpp20::constexprVirtualFunctionDemo();
+    std::cout << "**********************************************\n\n";
 
     // constinit
-    // cpp17::withoutConstinitDemo();
-    // cpp20::constinitDemo();
+    std::cout << "****************** constinit *****************\n";
+    cpp17::withoutConstinitDemo();
+    cpp20::constinitDemo();
+    std::cout << "**********************************************\n\n";
 
     // likely unlikely
-    // benchmark(cpp17::cos, "Without attributes");
-    // benchmark(cpp20::cos, "With attributes");
-    // benchmark<double(double)>(std::cos, "std::cos");
+    std::cout << "************** likely unlikely ***************\n";
+    benchmark(cpp17::cos, "Without attributes");
+    benchmark(cpp20::cos, "With attributes");
+    benchmark<double(double)>(std::cos, "std::cos");
+    std::cout << "**********************************************\n\n";
 
     // no_unique_address
+    std::cout << "************* no_unique_address **************\n";
     cpp17::emptyClassDemo();
     cpp20::emptyClassDemo();
+    std::cout << "**********************************************\n\n";
 
     // template lambda
+    std::cout << "**************** template lambda *************\n";
     cpp17::genericLambdaDemo();
     cpp20::templateLambdaDemo();
+    std::cout << "**********************************************\n\n";
 }
